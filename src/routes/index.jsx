bbeb9c9d4851063b1731623e21b1b606e5d72c70
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-05 23:02:55 -0300

    Add /routes index

---
+++ src/routes/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+// manager routes using Stack, can only acess 1 routes per time
+import { createStackNavigator } from '@react-navigation/stack';
+
+import AuthRoutes from './auth.routes';
+import TabRoutes from './app.routes';
+import StackTask from '../pages/StackTasks';
+
+import { useAuth } from '../hooks/auth';
+
+// Creating component with reactNavigation
+const Stack = createStackNavigator();
+
+// Name that we used at  navigator
+const RootNavigator = () => {
+  // calling the AuthHook, true ou false
+  const { user } = useAuth();
+
+  return (
+    <Stack.Navigator screenOptions={{
+      headerShown: false,
+    }}
+    >
+      <Stack.Screen name="Root" component={user ? TabRoutes : AuthRoutes} />
+      <Stack.Screen name="StackTasks" component={StackTask} />
+
+    </Stack.Navigator>
+  );
+};
+export default RootNavigator;
