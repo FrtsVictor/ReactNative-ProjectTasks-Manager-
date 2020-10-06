07b79c755744a8a204515cc84dcb06deaa7e5790
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-06 18:23:08 -0300

    Add StackNavigation inside TabNavigation /routes app

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,21 +1,49 @@
 import React from 'react';
 
+import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, AntDesign } from '@expo/vector-icons';
 
 import Project from '../pages/Projects';
-import Task from '../pages/ProjectTasks';
+import Dashboard from '../pages/Dashboard';
+
+import StackTasks from '../pages/ProjectTasks';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const ProjectRoutes = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      component={Project}
+      name="Project"
+      options={{
+        title: 'Project',
+        headerLeft: () => <AntDesign name="profile" size={50} color="white" />,
+        headerStyle: {
+          backgroundColor: '#457ae6',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          textAlignVertical: 'center',
+          fontWeight: 'bold',
+        },
+      }}
+    />
+    <Stack.Screen component={StackTasks} name="StackTasks" />
+  </Stack.Navigator>
+);
 
 const AppRoutes = () => (
 
   <Tab.Navigator
     initialRouteName="Project"
+
   >
+
     <Tab.Screen
       name="Project"
-      component={Project}
+      component={ProjectRoutes}
       options={{
         tabBarIcon: () => (
           <Ionicons name="ios-podium" size={32} color="black" />
@@ -24,8 +52,8 @@ const AppRoutes = () => (
     />
 
     <Tab.Screen
-      name="Task"
-      component={Task}
+      name="Dashboard"
+      component={Dashboard}
       options={{
         tabBarIcon: () => (
           <Ionicons name="ios-list-box" size={32} color="black" />
