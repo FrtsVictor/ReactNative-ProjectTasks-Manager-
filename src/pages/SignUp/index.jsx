d66283baac3b6049e1f6f176786674392c6e3ff1
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-08 16:16:23 -0300

    Add SignUp /pages

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import api from '../../services/api';
+
+import {
+  Container,
+  Image,
+  Input,
+  Button,
+  ButtonText,
+  AccountMessage,
+  GoBackTxt,
+} from './styles';
+
+import signUpImg from '../../assets/createaccount2.png';
+
+const SignUp = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [accountMessage, setAccountMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function createAccount() {
+    if (!email || !password) {
+      setAccountMessage('User or Password invalid');
+      return;
+    }
+
+    setLoading(true);
+
+    const response = await api.post('users', {
+      email,
+      password,
+    }).then(() => {
+      setPassword('');
+      setEmail('');
+      setAccountMessage('Account created Sucessfull');
+      return response;
+    }).catch((error) => {
+      setAccountMessage('Internal Server Error sorry :( ');
+      console.log(error);
+    })
+      .finally(setLoading(false));
+  }
+
+  useEffect(() => {
+    setAccountMessage('');
+  }, []);
+
+  return (
+    <>
+      <AntDesign.Button
+        name="left"
+        backgroundColor="#F0F0F5"
+        size={24}
+        color="black"
+        onPress={() => navigation.goBack()}
+      >
+        <GoBackTxt>Go back</GoBackTxt>
+      </AntDesign.Button>
+
+      <Container>
+
+        <Image source={signUpImg} />
+
+        <Input
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          placeholder="Login"
+        />
+
+        <Input
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          placeholder="Password"
+        />
+
+        {accountMessage !== '' ? (
+          <AccountMessage>{accountMessage}</AccountMessage>
+        ) : (
+          null
+        )}
+
+        <Button onPress={() => createAccount()}>
+          {loading ? (
+            <ActivityIndicator color="#fff" />
+          ) : (
+            <ButtonText>Create</ButtonText>
+          ) }
+        </Button>
+
+      </Container>
+    </>
+  );
+};
+
+export default SignUp;
