63c33d00d718eb5e5ed2bb743b20c4fec05f9807
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-07 22:15:46 -0300

    Add Button Navigation  Style /routes

---
+++ src/routes/styles.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const TitleIcon = styled.View`
+margin: 0 0 20px 20px;
+`;
+
+export const BtnView = styled.View`
+margin: 0 20px 20px 20px;
+`;
+
+export const BtnLogout = styled.TouchableOpacity`
+`;
+
+export const BtnTxt = styled.Text`
+color: #FFF;
+
+`;
