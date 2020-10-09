beebc8597f4384b3cff92997214c9b8662ddc6e8
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-09 19:47:10 -0300

    Add textInput  /components

---
+++ src/components/textInput.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export default styled.TextInput`
+flex:1;
+background-color: #f2feff;
+height: 40px;
+margin-top: 40px;
+padding: 0 20px;
+border-radius: 5px;
+color: #000;
+`;
