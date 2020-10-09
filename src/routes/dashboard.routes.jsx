56d335f93438863e3ad89a79e7f931349ec59d95
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-09 19:48:56 -0300

    Add logoutButton /routes dashboard.routes

--- src/routes/dashboard.routes.jsx
+++ src/routes/dashboard.routes.jsx
@@ -4,31 +4,47 @@ import { AntDesign } from '@expo/vector-icons';
 import { createStackNavigator } from '@react-navigation/stack';
 import Dashboard from '../pages/Dashboard';
 
-import { TitleIcon } from './styles';
+import {
+  TitleIcon,
+  BtnLogout, BtnTxt, BtnView,
+} from './styles';
+
+import { useAuth } from '../hooks/auth';
 
 const Stack = createStackNavigator();
 
-const DashboardRoutes = () => (
-  <Stack.Navigator>
-    <Stack.Screen
-      name="Dashboard"
-      component={Dashboard}
-      options={{
-        title: 'Dashboard',
-        headerLeft: () => <TitleIcon><AntDesign name="barschart" size={45} color="white" /></TitleIcon>,
-        headerStyle: {
-          backgroundColor: '#457ae6',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-          fontSize: 26,
-          marginBottom: 20,
-          marginLeft: 10,
-        },
-      }}
-    />
-  </Stack.Navigator>
-);
+const DashboardRoutes = () => {
+  const { signOut } = useAuth();
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          title: 'Dashboard',
+          headerLeft: () => <TitleIcon><AntDesign name="barschart" size={45} color="white" /></TitleIcon>,
+          headerRight: () => (
+            <BtnView>
+              <BtnLogout onPress={() => signOut()} title="Info" color="#fff">
+                <BtnTxt>Logout</BtnTxt>
+              </BtnLogout>
+            </BtnView>
+          ),
+          headerStyle: {
+            backgroundColor: '#457ae6',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize: 26,
+            marginBottom: 20,
+            marginLeft: 10,
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default DashboardRoutes;
